a76a2efc93f864f43afd0495bbaa6a3fffbd7aea
W45 <73011833+wgs45@users.noreply.github.com>
2023-10-25 08:32:52 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ScrollToTop from './components/ScrollToTop';
 import Home from './components/Home';
 import Free from './components/Free';
 import Clients from './components/Clients';
-import SuperRare from './components/superRare';
+import SuperRare from './components/SuperRare';
 import Release from './components/Release';
 import Like from './components/Like';
 import Signup from './components/Signup';
